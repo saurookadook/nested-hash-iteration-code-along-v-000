332d17abe30e13563d211274bd013cc667c57391
Andy Maskiell <maskiella@gmail.com>
2017-12-13 20:51:03 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,10 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     # binding.pry
     contact_details_hash.each do |attribute, data|
-      binding.pry
+      # binding.pry
+      if attribute == :favorite_ice_cream_flavors
+        binding.pry
+      end
     end
   end
 end
